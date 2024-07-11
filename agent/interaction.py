7e54aa00cbf4389a6dc5a66f040bf77e31f6d878
Amila Indika <amilaindika789@gmail.com>
2024-07-10 21:02:41 -0700

    Add api key and file path

--- agent/interaction.py
+++ agent/interaction.py
@@ -1,5 +1,6 @@
 import argparse, yaml, asyncio
 from Agent.agent import Agent
+import os
 
 parser = argparse.ArgumentParser(description='Process config.')
 parser.add_argument('--config', '-c', type=str, default="./config/config.yaml", help='path to config file')
@@ -9,7 +10,8 @@ with open(args.config, 'r') as f:
     config = yaml.load(f, Loader=yaml.Loader)
 
 config['interaction_mode'] = True
+config['Agent']['ChatGPT_1']['api_keys'] = [os.environ['OPENAI_API_KEY']]
 agent = Agent(config)
 while True:
     instruction = input('Enter your instruction: \n')
-    asyncio.run(agent.Instruction('', instruction))
+    asyncio.run(agent.Instruction('', instruction, file=config['path']['source_path']))
