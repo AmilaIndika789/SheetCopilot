0b39d11d7e1b60dbf6d30ca21d027352283a2d30
Amila Indika <amilaindika789@gmail.com>
2024-12-23 15:18:31 -0800

    Add 2 correct examples from run 14

--- agent/extract_intermediate_and_refined_responses.py
+++ agent/extract_intermediate_and_refined_responses.py
@@ -4,7 +4,7 @@ import pathlib
 import pandas as pd
 
 OUTPUT_DIR = "../output_dir"
-DATA_PATH = "../dataset/dataset_150_copy.xlsx"
+DATA_PATH = "../dataset/dataset_149_copy.xlsx"
 # %%
 evaluation_config_file = f"{OUTPUT_DIR}/eval_result.yaml"
 with open(evaluation_config_file, mode='r') as file:
